c65b082a2eb979ff075fb39081095fba7c92974a
abdo290 <abdo.soliman290@gmail.com>
2019-07-05 14:22:12 +0200

    all

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,34 @@
  
 import React from "react"
 import PropTypes from "prop-types"
+import { createGlobalStyle } from 'styled-components'
 import Navbar from '../components/Global/Navbar'
- 
+import Footer from '../components/Global/Footer'
 
 import "./layout.css"
 
- const Layout = ({children}) => {
-   return <React.Fragment>
-   <Navbar />
-   {children}
-   </React.Fragment>
- }
+const Layout = ({ children }) => (
+  <div>
+    <GlobalStyle />
+    <Navbar />
+    {children}
+    <Footer />
+  </div>
+)
 
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Open Sans', sans-serif;
+  color:#262626;
+  background:#fff;
+}
+`
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
